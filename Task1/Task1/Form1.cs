6b75b9881313d180163fa5e9b812c239952c8050
levkovich-polina <levkovich.polina@gmail.com>
2023-01-26 16:37:07 +0300

    Created a method that draws circles

--- Task1/Task1/Form1.cs
+++ Task1/Task1/Form1.cs
@@ -6,7 +6,9 @@ namespace Task1
     {
         private Timer _timer;
         Point cursor;
-        List<Circle> list = new List<Circle>();
+        List<Circle> circles = new List<Circle>();
+        List<Circle> circlesDelete = new List<Circle>();
+        SolidBrush color;
         public class Circle
         {
             public Point Center { get; set; }
@@ -33,11 +35,11 @@ namespace Task1
             if (Panel.Capture == true)
             {
                 Circle circle = new Circle(cursor, radius);
-                list.Add(circle);
-                if (list.Count == 1)
+                circles.Add(circle);
+                if (circles.Count == 1)
                 {
                     TimerCallback tm = new TimerCallback(Draw);
-                    _timer = new Timer(tm, num, 0, 20);
+                    _timer = new Timer(tm, num, 0, 5);
                 }
             }
         }
@@ -52,16 +54,67 @@ namespace Task1
 
         private void Panel_MouseDown(object sender, MouseEventArgs e)
         {
+            int count = 0;
             if (e.Button == MouseButtons.Left)
             {
                 Panel.Capture = true;
                 cursor = new Point(e.X, e.Y);
+                //count += 1;
+                //QuantityLabel.Text = count.ToString();
+                Random random = new Random();
+                SolidBrush pen = new SolidBrush(Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)));
+                color = pen;
             }
         }
 
         public void Draw(object obj)
         {
-         
+            radius++;
+            if (circles.Count >= 2)
+            {
+                for (int i = 0; i < circles.Count; i++)
+                {
+                    for (int n = 1; n < circles.Count; n++)
+                    {
+                        if (Math.Sqrt(Math.Pow(Math.Abs(circles[n].Center.X - circles[i].Center.X), 2) + Math.Pow(Math.Abs(circles[n].Center.Y - circles[i].Center.Y), 2)) <= circles[n].Radius + circles[i].Radius)
+                        {
+                            circlesDelete.Add(circles[i]);
+                            circlesDelete.Add(circles[n]);
+                        }
+                    }
+                }
+                for (int x = 0; x < circles.Count; x++)
+                {
+                    circlesDelete.RemoveAt(x);
+                    if (circles.Count == 0)
+                    {
+                        _timer.Dispose();
+                    }
+                }
+            }
+            for (int a = 0; a < circles.Count; a++)
+            {
+                if (Math.Abs(circles[a].Center.Y - 0) <= circles[a].Radius)
+                {
+                    circles.RemoveAt(a);
+                    Panel.Invalidate();
+                    if (circles.Count == 0)
+                    {
+                        _timer.Dispose();
+                    }
+                }
+                if (Math.Abs(circles[a].Center.Y - Panel.ClientSize.Height) <= circles[a].Radius)
+                {
+                    circles.RemoveAt(a);
+                    Panel.Invalidate();
+                    if (circles.Count == 0)
+                    {
+                        _timer.Dispose();
+                    }
+                }
+            }
+            Graphics g = Panel.CreateGraphics();
+            g.FillEllipse(color, cursor.X - radius, cursor.Y - radius, radius * 2, radius * 2);
         }
     }
 }

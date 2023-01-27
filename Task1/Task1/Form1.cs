32c595f202d758add019e28312a273632494bbd3
levkovich-polina <levkovich.polina@gmail.com>
2023-01-27 11:53:39 +0300

    Made an increase in the radius and drawing a circle

--- Task1/Task1/Form1.cs
+++ Task1/Task1/Form1.cs
@@ -50,47 +50,7 @@ namespace Task1
             {
                 _circles[k].Radius++;
             }
-            if (_circles.Count >= 2)
-            {
-                for (int i = 0; i < _circles.Count; i++)
-                {
-                    for (int n = 1; n < _circles.Count; n++)
-                    {
-                        var dx = _circles[n].Center.X - _circles[i].Center.X;
-                        var dy = _circles[n].Center.Y - _circles[i].Center.Y;
-                        var dRadius = _circles[n].Radius + _circles[i].Radius;
-                        if (dx * dx + dy * dy <= dRadius * dRadius)
-                        {
-                            circlesDelete.Add(_circles[i]);
-                            circlesDelete.Add(_circles[n]);
-                        }
-                    }
-                }
-                for (int x = 0; x < circlesDelete.Count; x++)
-                {
-                    _circles.Remove(circlesDelete[x]);
-                    g.Clear(Color.White);
-                    if (_circles.Count == 0)
-                    {
-                        _timer.Dispose();
-                    }
-                }
-            }
-            for (int a = 0; a < _circles.Count; a++)
-            {
-                var dx = _circles[a].Center.X;
-                var dy = _circles[a].Center.Y;
-                var dRadius = _circles[a].Radius;
-                if (dy - 0 <= dRadius || Math.Abs(dy - Panel.ClientSize.Height) <= dRadius || dx - 0 <= dRadius || Math.Abs(dx - Panel.ClientSize.Width) <= dRadius)
-                {
-                    _circles.RemoveAt(a);
-                    g.Clear(Color.White);
-                    if (_circles.Count == 0)
-                    {
-                        _timer.Dispose();
-                    }
-                }
-            }
+            
             for (int m = 0; m < _circles.Count; m++)
             {
                 var dx = _circles[m].Center.X;
